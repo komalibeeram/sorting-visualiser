01cbd568267ff214bc383d40ff20b0f532209e09
komalibeeram <komalibeeram699@gmail.com>
2022-07-31 08:12:26 -0700

    merge sort code

--- sort-visualiser/src/sortingAlgorithms/sortingAlgorithms.js
+++ sort-visualiser/src/sortingAlgorithms/sortingAlgorithms.js
@@ -1,20 +1,48 @@
-export const getMergeSortAnimations
+export function getMergeSortAnimations(array){
+    const animations = [];
+    if(array.length <= 1) return array;
+    const auxiliaryArray = array.slice();
+    mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
+    return animations;
+}
 
-export const mergeSort = (array, animationss = []) => {
-    if(array.length === 1) return array;
-    const middleIdx = Math.floor(array.length / 2);
-    const firstHalf = mergeSort(array.slice(0, middleIdx));
-    const secondHalf = mergeSort(array.slice(middleIdx));
-    const sortedArray = [];
-    let i = 0, j = 0;
-    while(i < firstHalf.length && j < secondHalf.length){
-        if(firstHalf[i] < secondHalf[j]){
-            sortedArray.push(firstHalf[i++]);
+function mergeSortHelper(mainArray, startIdx, endIdx, auxiliaryArray, animations){
+    if(startIdx === endIdx) return;
+    const middleIdx = Math.floor((startIdx + endIdx) / 2);
+    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+    mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
+    doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+}
+
+function doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations){
+    let k = startIdx;
+    let i = startIdx;
+    let j = middleIdx + 1;
+    while(i <= middleIdx && j <= endIdx){
+        // values we are comparing are pushed once to change color
+        animations.push([i,j]);
+        // values we are comparing are pushed again to revert their color
+        animations.push([i,j]);
+        if(auxiliaryArray[i] <= auxiliaryArray[j]){
+            // overwrite value at idx k in org array with the value at idx i in aux array
+            animations.push([k, auxiliaryArray[i]]);
+            mainArray[k++] = auxiliaryArray[i++];
         }else{
-            sortedArray.push(secondHalf[j++]);
+            // overwrite value at idx k in org array with the value at idx j in aux array
+            animations.push([k, auxiliaryArray[j]]);
+            mainArray[k++] = auxiliaryArray[j++];
         }
     }
-    while(i < firstHalf.length) sortedArray.push(firstHalf[i++]);
-    while(j < secondHalf.length) sortedArray.push(secondHalf[j++]);
-    return sortedArray;
+    while(i <= middleIdx){
+        animations.push([i,i]);
+        animations.push([i,i]);
+        animations.push([k, auxiliaryArray[i]]);
+        mainArray[k++] = auxiliaryArray[i++];
+    }
+    while(j <= endIdx){
+        animations.push([j,j]);
+        animations.push([j,j]);
+        animations.push([k, auxiliaryArray[j]]);
+        mainArray[k++] = auxiliaryArray[j++];
+    }
 }
